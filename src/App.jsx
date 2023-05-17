c10b46a5ac32c6ead0ad9c14316ebf0164ce5030
oshanvikasith <oshanvikasith1998@gmail.com>
2023-05-18 01:23:42 +0530

    Workout page creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import Hero from "./Components/Hero";
 import DataInsertion from "./Components/DateInsertion";
-
+import Workouts from "./Components/Workouts";
 function App() {
   return (
     <>
       <Hero />
-      <DataInsertion/>
+      <DataInsertion />
+      <Workouts />
     </>
   );
 }
