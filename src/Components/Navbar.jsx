4e4c055ddd6be6c2f14f15a7ec637c803709f502
Jayamaha007 <nadunjayamaha45@gmail.com>
2023-05-22 10:31:20 +0530

    Nav Bar Updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -136,7 +136,7 @@ export const Navbar = () => {
 
         <NavbarLinksBox>
           <NavLink variant="body2">Home</NavLink>
-          <NavLink variant="body2">Features</NavLink>
+          <NavLink variant="body2">Profile</NavLink>
           <NavLink variant="body2">Services</NavLink>
           <NavLink variant="body2">Tracker</NavLink>
           <NavLink variant="body2">Contact</NavLink>
